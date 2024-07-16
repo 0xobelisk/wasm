a4073e9624c456bc7f03e73dbb94576525766ebe
henryliu <17600047539@163.com>
2024-07-16 21:36:26 +0800

    update

--- external/sov-wasm/src/lib.rs
+++ external/sov-wasm/src/lib.rs
@@ -1,15 +1,20 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::{from_value, to_value};
+use serde_json::Value;
+
+
 use wasm_bindgen::prelude::*;
 
 use sov_modules_api::execution_mode::Zk;
 use sov_mock_zkvm::MockZkVerifier;
 use sov_risc0_adapter::Risc0Verifier;
 use sov_modules_api::default_spec::DefaultSpec;
-use sov_modules_api::transaction::{UnsignedTransaction};
+use sov_modules_api::transaction::{UnsignedTransaction,Transaction};
 pub type ZkSpec = DefaultSpec<Risc0Verifier, MockZkVerifier, Zk>;
 pub type UnsignedTx = UnsignedTransaction<ZkSpec>;
+pub type SignedTx = Transaction<ZkSpec>;
+
 
 #[wasm_bindgen]
 extern "C" {
@@ -24,31 +29,60 @@ struct MyData {
 
 #[wasm_bindgen]
 pub fn serialize_call(data: JsValue) -> Vec<u8> {
-    let data:Vec<u8> = from_value(data).unwrap();
-    let mut utx_bytes: Vec<u8> = Vec::new();
-    BorshSerialize::serialize(&data, &mut utx_bytes).unwrap();
-    utx_bytes
+    let data: Value = from_value(data).unwrap();
+    let serialized_data = serde_json::to_vec(&data).unwrap();
+    serialized_data
 }
 
 #[wasm_bindgen]
 pub fn deserialize_call(data: &[u8]) -> JsValue {
-    let data: MyData = MyData::try_from_slice(data).unwrap();
-    to_value(&data).unwrap()
+    let deserialized_data: Value = serde_json::from_slice(data).unwrap();
+    to_value(&deserialized_data).unwrap()
 }
 
 // #[wasm_bindgen]
-// pub fn serialize_unsigned_transaction(data: JsValue) -> Vec<u8> {
-//     let utx: UnsignedTx = from_value(data).unwrap();
-//     let mut utx_bytes: Vec<u8> = Vec::new();
-//     BorshSerialize::serialize(&utx, &mut utx_bytes).unwrap();
-//     utx_bytes
+// pub fn serialize_txDetails(data: JsValue) -> Vec<u8> {
+//     let data: TxDs = from_value(data).unwrap();
+//     let serialized_data = serde_json::to_vec(&data).unwrap();
+//     serialized_data
 // }
 //
 // #[wasm_bindgen]
-// pub fn deserialize_unsigned_transaction(data: &[u8]) -> JsValue {
-//     let utx: UnsignedTx = UnsignedTransaction::try_from_slice(data).unwrap();
-//     to_value(&utx).unwrap()
+// pub fn deserialize_txDetails(data: &[u8]) -> JsValue {
+//     let deserialized_data: Value = serde_json::from_slice(data).unwrap();
+//     to_value(&deserialized_data).unwrap()
 // }
+
+#[wasm_bindgen]
+pub fn serialize_unsigned_transaction(data: JsValue) -> Vec<u8> {
+    let utx: UnsignedTx = from_value(data).unwrap();
+    let mut utx_bytes: Vec<u8> = Vec::new();
+    BorshSerialize::serialize(&utx, &mut utx_bytes).unwrap();
+    utx_bytes
+}
+
+#[wasm_bindgen]
+pub fn deserialize_unsigned_transaction(data: &[u8]) -> JsValue {
+    let utx: UnsignedTx = UnsignedTransaction::try_from_slice(data).unwrap();
+    to_value(&utx).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn serialize_signed_transaction(data: JsValue) -> Vec<u8> {
+    let utx: SignedTx = from_value(data).unwrap();
+    let mut utx_bytes: Vec<u8> = Vec::new();
+    BorshSerialize::serialize(&utx, &mut utx_bytes).unwrap();
+    utx_bytes
+}
+
+#[wasm_bindgen]
+pub fn deserialize_signed_transaction(data: &[u8]) -> JsValue {
+    let utx: SignedTx = SignedTx::try_from_slice(data).unwrap();
+    to_value(&utx).unwrap()
+}
+
+
+
 
 
 #[wasm_bindgen]
--- index.ts
+++ index.ts
@@ -1,16 +1,91 @@
-import * as wasm from './external/sov-wasm/pkg-r'
+import * as wasm from './external/sov-wasm/pkg'
+import { Ed25519Keypair } from '@0xobelisk/sui-client';
+import { bytesToHex, remove0x } from '@metamask/utils';
+import { createHash } from 'crypto';
+import { bech32m } from '@scure/base';
 
+/**
+ * 将公钥转换成 bech32 地址
+ * @param pubkey 公钥 (hex 字符串)
+ * @returns bech32 地址
+ */
+function pubkeyToBech32(pubkey: string): string {
+    // 1. 对公钥进行 SHA-256 哈希处理
+    const sha256Hash = createHash('sha256')
+        .update(Buffer.from(pubkey, 'hex'))
+        .digest();
+
+    // 2. 将 SHA-256 哈希结果进行 bech32m 编码
+    const words = bech32m.toWords(sha256Hash);
+    const bech32Address = bech32m.encode('sov', words);
+
+    return bech32Address;
+}
+
+const cleanPublicKeyResponse = (metamaskPublicKeyResponse: any) => {
+    // Slicing is to skip the key byte flags prefix, which Metamask prepends the public key with.
+    return remove0x(metamaskPublicKeyResponse).slice(2);
+};
 
 (async () => {
 
-    // 创建一个 MyData 对象
-    const myData = { field1: "Hello", field2: 42 };
+    // // 创建一个 MyData 对象
+    // const myData = { field1: "Hello", field2: 42 };
+    //
+    // // 序列化对象
+    // const serializedData: Uint8Array = wasm.serialize(myData);
+    // console.log('Serialized data:', serializedData);
+    //
+    // // 反序列化数据
+    // const deserializedData: any = wasm.deserialize(serializedData);
+    // console.log('Deserialized data:', deserializedData);
 
-    // 序列化对象
-    const serializedData: Uint8Array = wasm.serialize(myData);
-    console.log('Serialized data:', serializedData);
+    const json_call = '{"bank":{"CreateToken":{"salt":11,"token_name":"sov-test-token","initial_balance":1000000,"minter_address":"sov15vspj48hpttzyvxu8kzq5klhvaczcpyxn6z6k0hwpwtzs4a6wkvqwr57gc","authorized_minters":["sov1l6n2cku82yfqld30lanm2nfw43n2auc8clw7r5u5m6s7p8jrm4zqrr8r94","sov15vspj48hpttzyvxu8kzq5klhvaczcpyxn6z6k0hwpwtzs4a6wkvqwr57gc"]}}}'
+    const serializeCall = wasm.serialize_call(json_call)
+    const deserializedCall = wasm.deserialize_call(serializeCall)
+    // console.log(serializeCall)
+    // console.log(deserializedCall)
 
-    // 反序列化数据
-    const deserializedData: any = wasm.deserialize(serializedData);
-    console.log('Deserialized data:', deserializedData);
+    let runtime_msg = serializeCall
+    let nonce = 0
+    let details = {
+        max_priority_fee_bips:0,
+        max_fee:10000,
+        gas_limit:null,
+        chain_id:4321
+    }
+    let unsigned_transaction = {
+        runtime_msg,
+        nonce,
+        details
+    }
+    let unsigned_tx = wasm.serialize_unsigned_transaction(unsigned_transaction)
+    let de_unsigned_tx = wasm.deserialize_unsigned_transaction(unsigned_tx)
+    //
+    // console.log(unsigned_tx)
+    // console.log(de_unsigned_tx)
+
+
+    const keypair = new Ed25519Keypair();
+
+    const pub_key = cleanPublicKeyResponse(bytesToHex(keypair.getPublicKey().toRawBytes()));
+    console.log(`Sui Public Key: ${pub_key}`);
+
+    const signature = await keypair.sign(unsigned_tx)
+    console.log("signature",signature)
+
+
+
+    const signed_Transaction = {
+        signature,
+        pub_key,
+        ...unsigned_transaction
+    };
+
+    let signed_tx = wasm.serialize_signed_transaction(signed_Transaction)
+    let de_signed_tx = wasm.deserialize_signed_transaction(signed_tx)
+
+    console.log(signed_tx)
+    console.log(de_signed_tx)
+
 })();
